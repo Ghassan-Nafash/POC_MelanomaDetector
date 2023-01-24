a4bf81494d8cc5aa66cd49c6f9505a28cf830e32
ghassan <ghassan1993@outlook.com>
2023-01-24 19:01:39 +0100

    detecting borders feature

--- source/Preprocessing/preprocess.py
+++ source/Preprocessing/preprocess.py
@@ -27,31 +27,14 @@ def display_img(title, input_img):
 
 
 def threshold(gray_input):
-    kernal = np.ones((5,5),np.uint8)
-    # print("size of the gray image= ",gray_input.shape)
-    # print("max value of the gray image: ",gray_input.max())
-    # plt.imshow(gray_input,cmap='gray')
-    # plt.show()
+    #kernal = np.ones((5,5),np.uint8)
     thr, thresh_img = cv2.threshold(gray_input, 120, gray_input.max(), cv2.THRESH_OTSU)
-    thr, thresh_img = cv2.threshold(gray_input, 50, gray_input.max(), cv2.THRESH_OTSU) # Anna Testing
-    # print("Otsu Thresh Value: ",thr)
-    # plt.imshow(thresh_img,cmap='gray')
-    # plt.show()
+    #thr, thresh_img = cv2.threshold(gray_input, 50, gray_input.max(), cv2.THRESH_OTSU) # Anna Testing
+
     noise = closing(thresh_img)
-    # plt.imshow(noise,cmap='gray')
-    # plt.show()
-    # thresh_img2 = cv2.adaptiveThreshold(noise, gray_input.max(), cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_OTSU, 11, 11)
-    
+
     thresh_img2 = cv2.adaptiveThreshold(noise, gray_input.max(), cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 11)
     
-    
-    # mask = cv2.erode(thresh_img2, kernal, iterations = 2)
-    # plt.imshow(mask)
-    # plt.show()
-    # plt.imshow(thresh_img2,cmap='gray')
-    # plt.show()
-    # result = cv2.addWeighted(src1=thresh_img,alpha=0.6,src2=thresh_img2,beta=0.4,gamma=0)
-    # result = - thresh_img - thresh_img2
     return thresh_img2
 
 
@@ -67,9 +50,8 @@ def DEM(input_img):
     kern_erode = np.ones((5,5), dtype=np.uint8)
     result = cv2.erode(input_img,kern_erode,iterations=1)
     result = cv2.dilate(result,kern_dialate,iterations=1)
-    #print(type(result))
-    #print(result.shape)
     result = cv2.medianBlur(result,3)
+
     return result
 
 def blur(input_img):
@@ -85,68 +67,40 @@ def gamma_correction(input_img, gamma):
     return result
 
 def detect_border_irruglarity(image):
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (7, 7), 32)
-    ret, thresh = cv2.threshold(blur, 120, blur.max(), cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-
-    # cv2.CHAIN_APPROX_SIMPLE -> It removes all redundant points and compresses the contour, thereby saving memory.
-    # CHAIN_APPROX_NONE -> all the boundary points are stored
-
-    #contours,hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    contours,hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-
-    # check this out:
-    # https://docs.opencv.org/4.x/dd/d49/tutorial_py_contour_features.html
+    contours, hierarchy = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     cnt = max(contours, key=cv2.contourArea)
 
     if len(cnt) > 4:
         # Fitting an Ellipse
         ellipse = cv2.fitEllipse(cnt)
-        #x,y,w,h = cv2.boundingRect(cnt)
-        #area = hsv[int(y+(0.3*h)):int(y+(0.8*h)),int((0.2*w)+x):int((0.7*w)+x)]
         ellipse_cnt = cv2.ellipse2Poly( (int(ellipse[0][0]),int(ellipse[0][1]) ) ,( int(ellipse[1][0]),int(ellipse[1][1]) ),int(ellipse[2]),0,360,1)
         comp = cv2.matchShapes(cnt, ellipse_cnt, 1, 0.0)
-        #variance = cv2.meanStdDev(area)
         print(comp)
-        #print(variance[1])
-        cv2.ellipse(image,ellipse,(0,255,0),2)
+        cv2.ellipse(image,ellipse,(255,255,255),2)
 
-    cv2.drawContours(image, cnt, -1, (0, 0, 255), 3)
+    cv2.drawContours(image, cnt, -1, (255,255,255), 3)
 
     return image
 
 
 def preproc_img():
-    # img_index = list(range(409, 410))  # 556 to index the last image
-    #ocular_images = os.listdir('../POC_MelanomaDetector/Dataset/ocular_dataset')
-    # ocular_images = [24360, 24371, 24400, 24408, 24409, 24431, 24435, 24450, 24489, 24490, 24308] # a few images with ocular
-
     img_index = list(range(306, 314))  # 556 to index the last image
     for i in img_index:
-    #for i in ocular_images:
         img_path = '../POC_MelanomaDetector/Dataset/ISIC_0024{}.jpg'.format(i)
-        #img_path = '../POC_MelanomaDetector/Dataset' + str(i)
-        #img_for_irr = cv2.imread("../POC_MelanomaDetector/Dataset/ISIC_0024306.jpg")
-        img_for_irr = cv2.imread(img_path)
         original_img = cv2.imread(img_path).astype(np.float32) / 255
         original_img_rgb = cv2.cvtColor(original_img, cv2.COLOR_BGR2RGB)
 
-
-        # print(original_img.max(), original_img.min())
-        # gamma_img = np.array(gamma_correction(original_img, gamma=0.8)*255).astype(np.uint8)  # choose gamma value
-        gamma_img = np.array(gamma_correction(original_img_rgb, gamma=1.3)*255).astype(np.uint8)  # choose gamma value Anna
+        gamma_img = np.array(gamma_correction(original_img_rgb, gamma=1.3)*255).astype(np.uint8)
         blurred_img = blur(gamma_img)
         gray_input = cv2.cvtColor(blurred_img, cv2.COLOR_RGB2GRAY)
         bin_img = threshold(gray_input)
-        #bin_img = DEM(bin_img)
+
         external_contours = find_contours(bin_img, original_img_rgb, ocular_detection=False).astype(np.uint8)
-        #external_contours_with_ocular_detection = find_contours(bin_img, original_img, ocular_detection=True).astype(np.uint8)
-        #external_contours = DEM(external_contours)
+
         external_contours_with_ocular_detection = find_contours(bin_img, original_img_rgb, ocular_detection=True).astype(np.uint8)
         # detecting border irruglarity
-        image_with_irrugular_border = detect_border_irruglarity(img_for_irr)
+        image_with_irrugular_border = detect_border_irruglarity(external_contours_with_ocular_detection)
         display_img("Image " + str(i), [original_img_rgb, gamma_img, blurred_img, gray_input, bin_img, 
                         external_contours, external_contours_with_ocular_detection, image_with_irrugular_border])
     
@@ -156,9 +110,7 @@ def preproc_img():
 
 def find_contours(input_img, original_img, ocular_detection=True):
     contour, hierarchy = cv2.findContours(input_img,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    #print('hierarchy', hierarchy)
     external_contours = np.zeros(input_img.shape)
-    #internal_contours = np.zeros(input_img.shape)
     longest_cntr = None
     largest_area = 0
     count = 0
@@ -174,6 +126,7 @@ def find_contours(input_img, original_img, ocular_detection=True):
                 longest_cntr = cntr
     if not longest_cntr is None:
         cv2.drawContours(external_contours,[longest_cntr],-1,255,2)
+
     return external_contours
 
 
@@ -192,15 +145,12 @@ def is_artificial_circle(contour, shape):
     hough_radii = [330]
     hough_res = sk.transform.hough_circle(mask, hough_radii)
 
-    # Select the most prominent 1 circle
     accums, cx, cy, radii = sk.transform.hough_circle_peaks(hough_res, hough_radii,
                                            total_num_peaks=1, normalize=False)
     non_zero = np.count_nonzero(mask)
-    # print(f"nonzero: {non_zero}") # debug info
-    # print(f"accums: {accums}") # debug info
-    # print("accums/non_zero: %.3f perc., radius: %d" %(accums[0]/non_zero * 100, radii[0] )) # debug info
+
     if accums[0]/non_zero > 0.005/100 and non_zero > 100:
-        # print(">>>>>> Occular detected <<<<<<<")  # debug info
+
         detected = True
 
     return detected
