caed7a3fa683a968b548cc1af65c50b2efbdc730
annadedo <41503831+annadedo@users.noreply.github.com>
2023-02-06 08:29:19 +0100

    Updated class structure for segmentation.py

--- source/segmentation/segmentation.py
+++ source/segmentation/segmentation.py
@@ -8,6 +8,7 @@ from skimage.segmentation import (morphological_chan_vese,
                                   morphological_geodesic_active_contour,
                                   inverse_gaussian_gradient,
                                   checkerboard_level_set)
+import os
 
 class Segmentation(ABC):
     """
@@ -30,15 +31,38 @@ class Segmentation(ABC):
             images[i] = img
         return images
 
-    def display():
+
+    def load_all(path: str):
         """
+        returns loaded images in RGB format as a dictionary 
+            - dict keys are image numbers from the dataset (last 5 digits)
+        """
+        images = dict()
+        valid_ext = [".jpg"]
+        for f in os.listdir(path):
+            filename = os.path.splitext(f)[0]
+            ext = os.path.splitext(f)[1]
+            if ext.lower() in valid_ext:
+                i = int(filename[-6:-1])
+                img = cv2. imread(os.path.join(path, f)).astype(np.float32) / 255
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                images[i] = img
+        return images
+
+
+    @abstractclassmethod
+    def display(image, cont):
+        """
+        To be implemented by every child class.
+        Displays the contour on original image.
         """
         pass
+
 
     @abstractclassmethod
     def segment(image):
         """
-        To be implemented by inheriting classes.
+        To be implemented by every child class.
         Returns contour
         """
         pass
@@ -46,18 +70,9 @@ class Segmentation(ABC):
 
 class MorphACWE(Segmentation):
     """
+    Class for segmentation with Morphological snakes Active contours without edges method.
+    Implements methods segment(...) and display(...) of the Segmentation class
     """
-    __evolution = []
-
-    def __store_evolution_in(lst):
-        """Returns a callback function to store the evolution of the level sets in
-        the given list.
-        """
-        def _store(x):
-            lst.append(np.copy(x))
-
-        return _store
-
     def segment(image) -> np.array:
         """
         segments single image
@@ -67,45 +82,49 @@ class MorphACWE(Segmentation):
 
         # Initial level set
         init_ls = checkerboard_level_set(image.shape, 6)
-        # List with intermediate results for plotting the evolution
-        __evolution = []
-        callback = MorphACWE.__store_evolution_in(__evolution)
+
         ls = morphological_chan_vese(image, num_iter=35, init_level_set=init_ls,
-                                    smoothing=3, iter_callback=callback)
+                                    smoothing=3)
         return ls
     
-    def display(level_set):
-        plt.imshow(level_set, cmap='gray')
+
+    def display(image, cont):
+        """
+        Implements the display method of Segmentation class.
+        Displays contour on the original image.
+        """
+        plt.imshow(image, cmap='gray')
         plt.axis('off')
-        # plt.axis('flatten')
-        plt.contour(level_set, [0.5], colors="r")
+        plt.contour(cont, [0.5], colors="cyan")
         plt.title("Morphological ACWE segmentation", fontsize=12)
         plt.show()
 
-    def display_intermediate(level_set):
-        """displays the intermediate steps of last execution of MorphACWE.segment"""
-        plt.imshow(level_set, cmap="gray")
-        plt.axis('off')
-        contour = plt.contour()
-        contour = plt.contour(MorphACWE.__evolution[2], [0.5], colors='g')
-        contour.collections[0].set_label("Iteration 2")
-        contour = plt.contour(MorphACWE.__evolution[7], [0.5], colors='y')
-        contour.collections[0].set_label("Iteration 7")
-        contour = plt.contour(MorphACWE.__evolution[-1], [0.5], colors='r')
-        contour.collections[0].set_label("Iteration" + str(len(MorphACWE.__evolution)-1))
-        plt.legend(loc="upper right")
-        title = "Morphological ACWE evolution"
-        plt.title(title, fontsize=12)
-        plt.show()
+
+class ColorFilter(Segmentation):
+    """
+    Class for segmentation with Color filter method.
+    To be implemented.
+    """
+    def segment(image) -> np.array:
+        """
+        To be implemented
+        """
+        pass
+    
+    def display(image, cont):
+        """
+        To be implemented
+        """
+        pass
 
 
 if __name__ == "__main__":
     start_index = 29422
     end_index = 29429
     images = Segmentation.load("C:/Users/ancik/Documents/GitHub/Dataset/", 29422, 29429)
+    # images = Segmentation.load_all("C:/Users/ancik/Documents/GitHub/Dataset/")
     for img_number in images.keys():
         img = images[img_number]
         seg = MorphACWE.segment(img)
-        MorphACWE.display(seg)
-        # MorphACWE.display_intermediate(seg)
+        MorphACWE.display(img, seg)
 
