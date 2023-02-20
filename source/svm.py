7511e350736d64204214bd77ed506ff099bc566e
YznLos <121551348+YznLos@users.noreply.github.com>
2023-02-20 09:49:34 +0100

    nothing is changed

--- source/svm.py
+++ source/svm.py
@@ -8,7 +8,6 @@ import numpy as np
 from sklearn import svm
 from sklearn import preprocessing
 from sklearn.preprocessing import StandardScaler
-import plottingSVM
 
 class Prediction():
     """
@@ -19,11 +18,12 @@ class Prediction():
         df_feat = data_frames[['f_a_0', 'f_a_1', 'f_a_2', 'f_a_3', 'f_b_0', 'f_c_0', 'f_c_1', 'f_c_2', 'f_c_3', 'f_c_4']]
 
         df_target = data_frames['metadata_label']
-
-
+        # df_feat = df_feat[['f_a_0', 'f_a_1', 'f_a_2', 'f_a_3']]
+        # df_feat = df_feat[['f_b_0', 'f_c_0', 'f_c_1', 'f_c_2', 'f_c_3', 'f_c_4']]
+        # print("df_feature_one_methode", df_feat)
         target_vector = np.ravel(df_target, order='C')                
         
-        x_train, x_test, y_train, y_test = train_test_split(df_feat, target_vector, test_size=0.30, random_state=1)
+        x_train, x_test, y_train, y_test = train_test_split(df_feat, target_vector, test_size=0.20, random_state=101)
 
         print("x_train_", x_train)
 
@@ -84,32 +84,29 @@ class Prediction():
         x_std = training_data[5]
 
         #param_grid = {'C': [0.1, 1, 10, 100, 1000], 'gamma': [1, 0.1, 0.01, 0.001, 0.0001], 'kernel': ['rbf']}
-        param_grid = {'C': [0.01, 0,1, 1, 10], 'gamma': [1, 0.1, 0.01, 0.001], 'kernel': ['linear','rbf']}
-        grid = GridSearchCV(SVC(), param_grid, refit=True, verbose=3)
+        # param_grid = {'C': [0.01, 0,1, 1, 10], 'gamma': [1, 0.1, 0.01, 0.001], 'kernel': ['linear','rbf']}
+        # grid = GridSearchCV(SVC(), param_grid, refit=True, verbose=3)
 
         #grid = GridSearchCV(SVC(), param_grid, refit=True, verbose=3)
-        #svm_rbf = SVC(kernel='rbf', C=0.1, gamma=1)
+        svm_rbf = SVC(kernel='rbf', C=1, gamma=0.01)
 
-        grid.fit(x_train, y_train)
+        svm_rbf.fit(x_train, y_train)
         
         normalized_test_data = Prediction.normalize_data_for_prediction(x_test, x_mean, x_std)
 
-        grid_predictions = grid.predict(normalized_test_data)
+        svm_rbf_predictions = svm_rbf.predict(normalized_test_data)
 
-        print("grid.best_params_=",grid.best_params_)
-        print("grid.best_score_=",grid.best_score_)
+        # print("grid.best_params_=",grid.best_params_)
+        # print("grid.best_score_=",grid.best_score_)
 
-        print(confusion_matrix(y_test, grid_predictions))
+        print(confusion_matrix(y_test, svm_rbf_predictions))
 
-        print(classification_report(y_test, grid_predictions))
+        print(classification_report(y_test, svm_rbf_predictions))
 
-        MSE = mean_squared_error(y_test, grid_predictions)
+        MSE = mean_squared_error(y_test, svm_rbf_predictions)
 
         print("MSE=", MSE)
-
-        
-
-        return grid_predictions
+        return svm_rbf_predictions
 
     def grid_search(training_data:list):        
         
